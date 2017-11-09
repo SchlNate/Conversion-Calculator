9b6ed591630424d5c95a82bea2408d6df3a3a693
Nathan Schlechte <nas4m7@mail.missouri.edu>
2017-11-08 22:59:50 -0600

    Added UI Elements

---
+++ Conversion Calculator/Conversion Calculator/Converter.swift
@@ -0,0 +1,15 @@
+//
+//  Converter.swift
+//  Conversion Calculator
+//
+//  Created by Nathan Schlechte on 11/8/17.
+//  Copyright © 2017 Nathan Schlechte. All rights reserved.
+//
+
+import Foundation
+
+struct Converter {
+    var label: String
+    var inputUnit: String
+    var outputUnit: String
+}
